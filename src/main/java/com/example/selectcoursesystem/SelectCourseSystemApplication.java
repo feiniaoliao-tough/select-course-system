8f11f97fdb4898add3bd99574ea2def356fce573
feiniaoliao <jikangliu9@gmail.com>
2026-05-15 10:29:25 +0800

    first commit

---
+++ src/main/java/com/example/selectcoursesystem/SelectCourseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.selectcoursesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectCourseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectCourseSystemApplication.class, args);
+    }
+
+}
